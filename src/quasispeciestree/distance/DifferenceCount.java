6fa9fd3392546855c965ddffd313245e1bfbd37f
boskovav <2584478+boskovav@users.noreply.github.com>
2017-03-09 17:04:40 +0100

    Class for reading in the full newick tree and transforming it to qsTree

---
+++ src/quasispeciestree/distance/DifferenceCount.java
@@ -0,0 +1,24 @@
+package quasispeciestree.distance;
+
+import beast.core.Description;
+import beast.evolution.alignment.distance.Distance;
+
+/**
+ * Created by boskovav on 03/03/17.
+ */
+
+@Description("Counts the simple number of characters that differ between sequences. " +
+             "Note that unknowns are not ignored, so if both are unknowns '?' the distance is zero.")
+public class DifferenceCount extends Distance.Base {
+
+        @Override
+        public double pairwiseDistance(int taxon1, int taxon2) {
+            double dist = 0;
+            for (int i = 0; i < patterns.getPatternCount(); i++) {
+                if (patterns.getPattern(taxon1, i) != patterns.getPattern(taxon2, i)) {
+                    dist += patterns.getPatternWeight(i);
+                }
+            }
+            return dist;  // / patterns.getSiteCount();
+        }
+}
---
+++ src/quasispeciestree/tree/QuasiSpeciesTreeFromFullNewick.java
@@ -0,0 +1,58 @@
+package quasispeciestree.tree;
+
+import beast.core.*;
+import beast.core.Input.Validate;
+import beast.evolution.alignment.Alignment;
+import beast.evolution.tree.Tree;
+import beast.util.TreeParser;
+
+import java.util.List;
+
+/**
+ * @author Veronika Boskova created on 02/03/2017.
+ */
+
+@Description("Class to initialize a QuasiSpeciesTree from the full tree in newick tree format")
+public class QuasiSpeciesTreeFromFullNewick extends QuasiSpeciesTree implements StateNodeInitialiser{
+
+    final public Input<Alignment> dataInput = new Input<>("data",
+            "Alignment data used for calculating distances for clustering",
+            Input.Validate.REQUIRED);
+    public Input<String> newickStringInput = new Input<>("newick",
+            "Tree in Newick format.", Input.Validate.REQUIRED);
+    public Input<Boolean> adjustTipHeightsInput = new Input<>("adjustTipHeights",
+            "Adjust tip heights in tree? Default true.", true);
+
+    public QuasiSpeciesTreeFromFullNewick() {
+
+        // When specifying the input tree with full newick tree, we do not allow for duplicate counts input on the top.
+        haplotypeCountsInput.setRule(Input.Validate.FORBIDDEN);
+
+    }
+
+    public void initAndValidate() {
+        super.initAndValidate();
+
+        TreeParser inputTree = new TreeParser();
+        inputTree.initByName(
+                "IsLabelledNewick", true,
+                "adjustTipHeights", adjustTipHeightsInput.get(),
+                "newick", newickStringInput.get());
+
+        List<String> taxaNames;
+        if (dataInput.get() == null)
+            throw new RuntimeException("The data input needs to be specified");
+
+        initFromRegularTree(inputTree,dataInput.get());
+
+    }
+
+    @Override
+    public void initStateNodes(){
+    }
+
+    @Override
+    public void getInitialisedStateNodes(List<StateNode> stateNodes) {
+        stateNodes.add(this);
+    }
+}
