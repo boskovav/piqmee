4c847405668a0406fbc0cb24045b53319c672ff6
boskovav <2584478+boskovav@users.noreply.github.com>
2020-01-08 16:23:21 +0100

    UC relaxed clock model categories logger for PIQMEE - for testing

---
+++ src/piqmee/evolution/branchratemodel/CategoriesLogger.java
@@ -0,0 +1,91 @@
+package piqmee.evolution.branchratemodel;
+
+import beast.core.*;
+import beast.evolution.branchratemodel.BranchRateModel;
+import beast.evolution.likelihood.GenericTreeLikelihood;
+import beast.evolution.tree.Tree;
+
+import java.io.PrintStream;
+
+
+
+@Description("Logger reporting categories for each branch of the tree.")
+public class CategoriesLogger extends BEASTObject implements Loggable, Function {
+
+    final public Input<GenericTreeLikelihood> likelihoodInput = new Input<>("treeLikelihood", "TreeLikelihood containing branch rate model that provides rates for a tree");
+    final public Input<BranchRateModel> branchRateModelInput = new Input<>("branchratemodel", "model that provides rates for a tree", Input.Validate.XOR, likelihoodInput);
+    final public Input<Tree> treeInput = new Input<>("tree", "tree for which the rates apply");
+
+    private Tree tree = null;
+
+    private BranchRateModel branchRateModel = null;
+
+    static int[] categories;
+
+    @Override
+    public void initAndValidate() {
+        tree = treeInput.get();
+        branchRateModel = branchRateModelInput.get();
+        if (branchRateModel == null) {
+            branchRateModel = likelihoodInput.get().branchRateModelInput.get();
+        }
+
+        categories = new int [tree.getNodeCount()+tree.getLeafNodeCount()];
+        for (int i=0;i<tree.getNodeCount()+tree.getLeafNodeCount();i++){
+            categories[i]= ((QuasiSpeciesUCRelaxedClockModel) branchRateModel).getCategories(i);
+        }
+    }
+
+    @Override
+    public int getDimension() {
+        return categories.length;
+    }
+
+    @Override
+    public double getArrayValue() {
+        return categories[0];
+    }
+
+    @Override
+    public double getArrayValue(final int dim) {
+        if (dim > 3) {
+            throw new IllegalArgumentException();
+        }
+        return categories[dim];
+    }
+
+    /**
+     * Loggable implementation *
+     */
+
+    @Override
+    public void init(final PrintStream out) {
+        String id = getID();
+        if (id == null) {
+            id = "";
+        }
+        final int valueCount = getDimension();
+        if (valueCount == 1) {
+            out.print(getID() + "\t");
+        } else {
+            for (int value = 0; value < valueCount; value++) {
+                out.print(getID() + (value + 1) + "\t");
+            }
+        }
+    }
+
+    public void log(final long sample, final PrintStream out) {
+        final int valueCount = getDimension();
+        for (int i=0;i<tree.getNodeCount()+tree.getLeafNodeCount();i++){
+            categories[i]= ((QuasiSpeciesUCRelaxedClockModel) branchRateModel).getCategories(i);
+        }
+        for (int i = 0; i < valueCount; i++) {
+            out.print(categories[i] + "\t");
+        }
+    }
+
+    @Override
+    public void close(final PrintStream out) {
+        // nothing to do
+    }
+}
