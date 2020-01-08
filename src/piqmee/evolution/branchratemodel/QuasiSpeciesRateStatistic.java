48c4a60ad2a5da28a7c42697de7ae5f58de694f7
boskovav <2584478+boskovav@users.noreply.github.com>
2020-01-08 16:52:25 +0100

    RateStatistic class changed to return correct statistic

--- src/piqmee/evolution/branchratemodel/QuasiSpeciesRateStatistic.java
+++ src/piqmee/evolution/branchratemodel/QuasiSpeciesRateStatistic.java
@@ -62,11 +62,17 @@ public class QuasiSpeciesRateStatistic extends RateStatistic {
         }
         if (internal) {
             length += tree.getInternalNodeCount() - 1 + nrOfQSDupl;
-            lengthBL += tree.getInternalNodeCount() - 1 + nrOfLeafs;
+            // nrOfLeafs is 2 * because
+            //      1) for the branches above haplo, when haplo starts above a tip and
+            //      2) for internal QS branches
+            lengthBL += tree.getInternalNodeCount() - 1 + 2 * nrOfLeafs;
         }
 
         // these are the rates for each branch as in the full tree when reconstructed from the QS tree
         final double[] rates = new double[length];
+        // these are the rates for each branch as in the full tree when reconstructed from the QS tree in case one haplo
+        //  is above the root --- and so we have one less rate (no partial branch rate above that haplo)
+        final double[] ratestmp = new double[length-1];
         // for internal QS branches and lengths...need to split from external QS branch lengths
 //        final double[] internalQSBranchLengths = new double[nrOfLeafs];
 //        final double[] internalQSBranchRates = new double[nrOfLeafs];
@@ -91,15 +97,18 @@ public class QuasiSpeciesRateStatistic extends RateStatistic {
                 double[] attachTimes = child.getAttachmentTimesList();
                 int attachTimeArrayLength = attachTimes.length;
                 // count here only the QS subtrees without the partial branch above it!!!
-                // it can be that there are no duplicates, so the branch is from the child to the parent
+                // 1) it can be that there are no duplicates, so the branch is from the child to the parent
+                //      and has a rate of tree.getNodeCount()+child.getNr()
                 if (haploAboveChild != -1 && attachTimeArrayLength == 1) {
                     branchLengths[i] = parent.getHeight() - child.getHeight();
+                    toyNode.setNr(tree.getNodeCount()+child.getNr());
+                    rate = branchRateModel.getRateForBranch(toyNode);
                     rates[k] = rate;
                     k++;
                 }
-                // and now for QS duplicates
+                // 2) and now for QS with duplicates
                 else {
-                    branchLengths[i] = child.getTotalBranchLengths();
+                    branchLengths[i] = child.getTotalBranchLengths() - (attachTimes[0]-attachTimes[attachTimeArrayLength-1]);
 
                     // for each branch of the haplotype add contribution to rates array (also for 0 - first = "true" branch)
                     for (int q = 0; q < attachTimeArrayLength; q++) {
@@ -127,20 +136,28 @@ public class QuasiSpeciesRateStatistic extends RateStatistic {
                 // check if the partial branch is above the tip
                 //      (else above another internal node --- treated in internal node loop)
                 if (attachTimeArrayLength > 1){
-                   if (attachTimes[0] < child.getParent().getHeight()){
-                        toyNode.setNr(tree.getNodeCount()+child.getNr());
-                        rate = branchRateModel.getRateForBranch(toyNode);
-                        branchLengths[l] = child.getParent().getHeight() - attachTimes[0];
-                        ratesForBranchLengths[l] = rate;
-                        rates[k] = rate;
-                        k++;
-                        l++;
-                    }
                     // this is for the QS duplicate branches and respective internal QS branches
                     for (int q = 0; q < attachTimeArrayLength - 2; q++) {
                         rates[k + q] = rate;
                     }
                     k += attachTimeArrayLength - 2;
+
+                    if (attachTimeArrayLength > 2) {
+                        branchLengths[l] = attachTimes[0] - attachTimes[attachTimeArrayLength - 1];
+                        ratesForBranchLengths[l] = rate;
+                        l++;
+                    }
+                    // if above the tip, it has toyNode branchRate
+                    if (attachTimes[0] < child.getParent().getHeight()){
+                        toyNode.setNr(tree.getNodeCount()+child.getNr());
+                        rate = branchRateModel.getRateForBranch(toyNode);
+                        rates[k] = rate;
+                        k++;
+
+                        branchLengths[l] = child.getParent().getHeight() - attachTimes[0];
+                        ratesForBranchLengths[l] = rate;
+                        l++;
+                    }
                 }
             }
 
@@ -155,17 +172,22 @@ public class QuasiSpeciesRateStatistic extends RateStatistic {
                     double rate = branchRateModel.getRateForBranch(child);
                     // Case 1: branch can be free - i.e. no haplo passing, branch has its rate
                     if (haploContChild == -1) {
+                        rates[k] = rate;
+
                         branchLengths[l] = parentHeight - child.getHeight();
                         ratesForBranchLengths[l] = rate;
-                        rates[k] = rate;
                         l++;
                     }
                     // Case 2: it can be that a haplo starts above this node, in that case
                     //          only the partial branch has the branch's rate
                     else if (haploAboveChild != -1) {
+                        rates[k] = rate;
+                        k++;
+                        // and one of the haplo's branches was split in two
+                        rates[k] = branchRateModel.getRateForBranch(tree.getNode(haploAboveChild));
+
                         branchLengths[l] = parentHeight - ((QuasiSpeciesNode) tree.getNode(haploAboveChild)).getAttachmentTimesList()[0];
                         ratesForBranchLengths[l] = rate;
-                        rates[k] = rate;
                         l++;
                     }
                     // Case 3: it can be that a haplo passes this branch, in that case the rate is the same as for the
@@ -195,9 +217,18 @@ public class QuasiSpeciesRateStatistic extends RateStatistic {
         //  final double mean = DiscreteStatistics.mean(rates);
         //        values[VARIANCE] = DiscreteStatistics.variance(rates, mean);
         //        values[COEFFICIENT_OF_VARIATION] = Math.sqrt(D values[VARIANCE]) / mean;
-        values[VARIANCE] = DiscreteStatistics.variance(ratesForBranchLengths);
-        final double mean = DiscreteStatistics.mean(ratesForBranchLengths);
-        values[COEFFICIENT_OF_VARIATION] = Math.sqrt(DiscreteStatistics.variance(ratesForBranchLengths, mean)) / mean;
+
+        // if a haplo is above a root, we do not have a partial branch rate for the branch above that haplo
+        if (((QuasiSpeciesNode)tree.getRoot()).getHaploAboveName() != -1){
+            System.arraycopy(rates,0,ratestmp, 0, length -1);
+            values[VARIANCE] = DiscreteStatistics.variance(ratestmp);
+            final double mean = DiscreteStatistics.mean(ratestmp);
+            values[COEFFICIENT_OF_VARIATION] = Math.sqrt(DiscreteStatistics.variance(ratestmp, mean)) / mean;
+        } else {
+            values[VARIANCE] = DiscreteStatistics.variance(rates);
+            final double mean = DiscreteStatistics.mean(rates);
+            values[COEFFICIENT_OF_VARIATION] = Math.sqrt(DiscreteStatistics.variance(rates, mean)) / mean;
+        }
         return values;
     }
 
