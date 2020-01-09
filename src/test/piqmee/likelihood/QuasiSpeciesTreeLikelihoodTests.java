c858b7d23bef9878675cf4a20801b7595df87751
boskovav <2584478+boskovav@users.noreply.github.com>
2020-01-09 15:38:21 +0100

    Added a test for relaxed clock with gamma categories CLASSIC vs PIQMEE - this test is commented out - needs some functions in BEAST to be made public to work

--- src/test/piqmee/likelihood/QuasiSpeciesTreeLikelihoodTests.java
+++ src/test/piqmee/likelihood/QuasiSpeciesTreeLikelihoodTests.java
@@ -4,6 +4,7 @@ import beast.core.parameter.RealParameter;
 import beast.evolution.alignment.Alignment;
 import beast.evolution.branchratemodel.BranchRateModel;
 import beast.evolution.branchratemodel.UCRelaxedClockModel;
+import beast.evolution.likelihood.BeerLikelihoodCore;
 import beast.evolution.likelihood.TreeLikelihood;
 import beast.evolution.sitemodel.SiteModel;
 import piqmee.evolution.branchratemodel.QuasiSpeciesUCRelaxedClockModel;
@@ -11,6 +12,7 @@ import beast.evolution.substitutionmodel.JukesCantor;
 import beast.evolution.tree.Tree;
 import beast.util.TreeParser;
 import org.junit.Test;
+import piqmee.likelihood.QuasiSpeciesBeerLikelihoodCore;
 import piqmee.likelihood.QuasiSpeciesTreeLikelihood;
 import piqmee.tree.QuasiSpeciesNode;
 import piqmee.tree.QuasiSpeciesTree;
@@ -294,7 +296,7 @@ public class QuasiSpeciesTreeLikelihoodTests {
         }
 
         QuasiSpeciesUCRelaxedClockModel branchModel = new QuasiSpeciesUCRelaxedClockModel();
-        branchModel.initByName("distr",uldistr,"rateCategories","11","numberOfDiscreteRates","3","tree",tree);
+        branchModel.initByName("distr",uldistr,"clock.rate","9.6561609453625","rateCategories","11","numberOfDiscreteRates","3","tree",tree);
         // from the CLASSIC model above we have
         // tips: t0=2,t1=2,t2=0,t3=0,t4=0,t5=0,t6=0
         //
@@ -321,10 +323,101 @@ public class QuasiSpeciesTreeLikelihoodTests {
 
         // compare the two
         assertEquals(logP, -5.704853191310973, BEASTTestCase.PRECISION);
-        assertEquals(logQSP, -8.284978618736746, BEASTTestCase.PRECISION);
+        assertEquals(logQSP, -5.704910906518559, BEASTTestCase.PRECISION);
 
         assertTrue(logP > logQSP);
     }
+
+    /**
+     * test at the ML value for the CLASSIC model
+     * FOR THIS TEST WE NEED TO MAKE calculateIntegratePartials IN LikelihoodCore AND IN BeerLikelihoodCore CLASS
+     *  AND likelihoodCore IN TreeLikelihood CLASS PUBLIC
+     */
+//    @Test
+//    public void testJC69AndRelaxedClockLikelihoodMLforCLASSIC1() throws Exception {
+//        // Set up JC69 model: uniform freqs, kappa = 1, 0 gamma categories
+//        QuasiSpeciesTree tree = QuasiSpeciesTestCase.setTreeFromFullNewick("(((t3:1.,t4:1.):2.0,(t5:1.5,t6:0.5):0.5):1.0,((t0:1.5,t1:0.5):1.,t2:0.5):1.5);", new String[] {"C", "G", "T", "A", "A", "A", "A"});
+//        Tree treeNormal = new TreeParser("(t3:1.0,((t0:1.5,t1:0.5):1.,t2:0.5):1.5);",false);
+//
+//        Alignment data = QuasiSpeciesTestCase.getAlignment(new String[] {"C", "G", "T", "A", "A", "A", "A"});
+//        Alignment dataNormal = QuasiSpeciesTestCase.getAlignment(new String[] {"C", "G", "T", "A"});
+//
+//        JukesCantor JC = new JukesCantor();
+//        JC.initAndValidate();
+//
+//        SiteModel siteModel = new SiteModel();
+//        siteModel.initByName("mutationRate", "1.0", "gammaCategoryCount", "4", "shape", "3.4870306588330533", "substModel", JC);
+//
+//        LogNormalDistributionModel uldistr = new LogNormalDistributionModel();
+//        uldistr.initByName("M","1.0","S","3.0427110046894157","meanInRealSpace",true);
+//
+//        UCRelaxedClockModel branchModelNormal = new UCRelaxedClockModel();
+//        branchModelNormal.initByName("distr",uldistr,"clock.rate","9.6561609453625","rateCategories","7","numberOfDiscreteRates","3","tree",treeNormal);
+//        //from log file categories for branches are 2,2,0,0,0,0,0,2,2,0,0,2,0
+//        // branches are t0,t1,t2,t3,t4,t5,t6, 7(parent t0,t1),8(parent 7,t2), 9(parent t3,t4), 10(parent t5,t6), 11(parent 9,10), 12 root
+//        //
+//        // order of branches in our read in tree is different, so we need to shuffle the rates
+//        // order of branches here is:
+//        //              t0,t1,t2,t3,t4,t5,t6, 7(parent t3,t4),8(parent t5,t6), 9(parent 7,8), 10(parent t0,t1), 11(parent 10,t2), 12 root
+//        //
+//        // thus new order of categories is          2,2,0,0,0,0,0,0,0,2,2,2,0
+//        int[] categoriesNormal = {2,2,0,2,2,2,0};
+//        for (int i=0; i<6; i++) {
+//            branchModelNormal.setCategories(i,categoriesNormal[i]);
+//        }
+//
+//        QuasiSpeciesUCRelaxedClockModel branchModel = new QuasiSpeciesUCRelaxedClockModel();
+//        branchModel.initByName("distr",uldistr,"clock.rate","9.6561609453625","rateCategories","11","numberOfDiscreteRates","3","tree",tree);
+//        // from the CLASSIC model above we have
+//        // tips: t0=2,t1=2,t2=0,t3=0,t4=0,t5=0,t6=0
+//        //
+//        // internal nodes: 7(parent of t3,t4)=0; 8(parent of t5,t6)=0, 9(parent of 7,8 === partial branch above A)=2,
+//        //                 10(parent of t0,t1)=2, 11(parent of 10,t2)=2, 12 (root of the tree)=0
+//        // average for subtree of haplotype A == 2.5*0+4*0=0 === so first catefory of 3 --- so category 0
+//        //                                       here 2.5 is the branch length of branch 7+8 and 4 is the branch length of 3+4+5+6
+//        int[] categories = {0,0,0,0,2,2,0,2,2,0,2};
+//        for (int i=0; i<11; i++) {
+//            branchModel.setCategories(i,categories[i]);
+//        }
+//
+//        // QS likelihood
+//        QuasiSpeciesTreeLikelihood likelihood = newQSTreeLikelihood();
+//        likelihood.initByName("data", data, "tree", tree, "siteModel", siteModel, "branchRateModel", branchModel);
+//        double logQSP = 0;
+//        logQSP = likelihood.calculateLogP();
+//
+//        // normal likelihood
+//        TreeLikelihood likelihoodNormal = newTreeLikelihood();
+//        likelihoodNormal.initByName("data", dataNormal, "tree", treeNormal, "siteModel", siteModel, "branchRateModel", branchModelNormal);
+//        double logP = 0;
+//        logP = likelihoodNormal.calculateLogP();
+//
+//        double[] rates = new double[4];
+//        likelihood.getNoChangeRates(rates);
+//
+//        // compare the two
+//        QuasiSpeciesNode newNode = new QuasiSpeciesNode();
+//        newNode.setNr(3);
+//        double[] finalPartials = new double[4];
+//        double[] rootPartials = new double[16];
+//        double[] partials = new double[16];
+//        likelihoodNormal.likelihoodCore.getNodePartials(treeNormal.getRoot().getNr(),partials);
+//        int v=0;
+//        for (int i=0; i<4; i++) {
+//            for (int j=0; j < siteModel.getCategoryCount(); j++) {
+//                rootPartials[v] = partials[v] * Math.exp(rates[1] * 6.5 * branchModel.getRateForBranch(newNode) * siteModel.getCategoryRates(newNode)[i]);
+//                v++;
+//            }
+//        }
+//
+//        final double[] proportions = siteModel.getCategoryProportions(treeNormal.getRoot());
+//        likelihoodNormal.likelihoodCore.calculateIntegratePartials(rootPartials, proportions, finalPartials);
+//        double[] frequencies = siteModel.substModelInput.get().getFrequencies();
+//        double[] logPnew = new double[1];
+//        likelihoodNormal.likelihoodCore.calculateLogLikelihoods(finalPartials, frequencies, logPnew);
+//
+//        assertEquals(logPnew[0], logQSP, BEASTTestCase.PRECISION);
+//    }
 
     /**
      * test at the ML value for the CLASSIC model
