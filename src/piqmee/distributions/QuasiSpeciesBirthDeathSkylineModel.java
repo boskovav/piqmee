boskovav/piqmee

commit def3799e119271deb3094f7aff61c88149546259

speeding up processFirstProductTerm method by recalculating only parts that actually changed

src/piqmee/distributions/QuasiSpeciesBirthDeathSkylineModel.java
@@ -738,15 +738,35 @@ public class QuasiSpeciesBirthDeathSkylineModel extends BirthDeathSkylineModel {
 	            double[] QSTimesTemp = ((QuasiSpeciesNode) node).getAttachmentTimesList();
 	            //double[] QSTipTimesTemp = ((QuasiSpeciesNode) node).getTipTimesList();
 	            double temp = 0;
-	            for (int j = nQSTemp - 1; j > 0; j--) {
-	                double x = times[totalIntervals - 1] - QSTimesTemp[j];
-	                final int index = index(x);
-	                temp += FastMathLog(birth[index]) + log_q(index, times[index], x);
-	                if (printTempResults)
-	                    System.out.println("1st pwd" + " = " + temp + "; QSinterval & QS attachment branches = " + node.getID() + " " + j);
-	            }
-                logP += temp;
-                currentFirstTerms[node.getNr()] = temp;
+
+	            if (!bdskyIsDirty && !Double.isNaN(currentFirstTerms[node.getNr()]) && ((QuasiSpeciesNode) node).getOldtimeofchangedcopy()!=-1){
+                    //deduction fof contribution from old time
+	                double x = times[totalIntervals - 1] - ((QuasiSpeciesNode) node).getOldtimeofchangedcopy();
+                    int index = index(x);
+                    temp -= FastMathLog(birth[index]) + log_q(index, times[index], x);
+                    ((QuasiSpeciesNode) node).setOldtimeofchangedcopy(-1);
+                    //addition of contribution from new time
+                    x = times[totalIntervals - 1] - ((QuasiSpeciesNode) node).getNewtimeofchangedcopy();
+                    index = index(x);
+                    temp += FastMathLog(birth[index]) + log_q(index, times[index], x);
+                    ((QuasiSpeciesNode) node).setNewtimeofchangedcopy(-1);
+
+                    if (printTempResults)
+                        System.out.println("1st pwd changed by" + " = " + temp + "; QSinterval & QS attachment branches = " + node.getID());
+
+                    currentFirstTerms[node.getNr()] += temp;
+                    logP += currentFirstTerms[node.getNr()];
+                } else {
+                    for (int j = nQSTemp - 1; j > 0; j--) {
+                        double x = times[totalIntervals - 1] - QSTimesTemp[j];
+                        final int index = index(x);
+                        temp += FastMathLog(birth[index]) + log_q(index, times[index], x);
+                        if (printTempResults)
+                            System.out.println("1st pwd" + " = " + temp + "; QSinterval & QS attachment branches = " + node.getID() + " " + j);
+                    }
+                    logP += temp;
+                    currentFirstTerms[node.getNr()] = temp;
+                }
                 if (Double.isInfinite(logP)) {
                     return;
                 }

src/piqmee/operators/QuasiSpeciesSequenceAttachmentRandom.java
@@ -187,11 +187,15 @@ public class QuasiSpeciesSequenceAttachmentRandom extends QuasiSpeciesTreeOperat
             else{
                 tnewQSstart = tempqstimes[2];
             }
+            node.setNewtimeofchangedcopy(tnewQSstart);
+            node.setOldtimeofchangedcopy(tempqstimes[0]);
             tempqstimes[0] = tnewQSstart;
         }
         // creation of new 1st attachment time
         else if (tmaxIdx==0){
             tnewQSstart = tnew;
+            node.setNewtimeofchangedcopy(tnewQSstart);
+            node.setOldtimeofchangedcopy(tempqstimes[0]);
             tempqstimes[0] = tnewQSstart;
         }
 

src/piqmee/operators/QuasiSpeciesSequenceAttachmentUniform.java
@@ -140,6 +140,9 @@ public class QuasiSpeciesSequenceAttachmentUniform extends QuasiSpeciesTreeOpera
         if (changeIdx == 1){
             tempqstimes[0] = tnew;
         }
+        node.setNewtimeofchangedcopy(tnew);
+        node.setOldtimeofchangedcopy(told);
+
         node.setAttachmentTimesList(tempqstimes);
 
         // account for the fact that we are changing the QS start

src/piqmee/tree/QuasiSpeciesNode.java
@@ -16,6 +16,12 @@ public class QuasiSpeciesNode extends QuasiSpeciesNodeBaseClass {
     protected double[] attachmentTimesList;
     boolean attachmentTimesListChanged;
 
+    // variables storing old and new attachment time if only a single one was changed via operators
+    //  -1 if not changed
+    double oldtimeofchangedcopy=-1;
+    double newtimeofchangedcopy=-1;
+
+
     public QuasiSpeciesNode() {}
 
     public QuasiSpeciesNode(final String id) {
@@ -258,6 +264,42 @@ public class QuasiSpeciesNode extends QuasiSpeciesNodeBaseClass {
                 k--;
             }
         }
+    }
+
+    /**
+     * Obtain the old time of changed haplotype copy
+     *
+     * @return old time
+     */
+    public double getOldtimeofchangedcopy() {
+        return this.oldtimeofchangedcopy;
+    }
+
+    /**
+     * Sets the old time of changed haplotype copy
+     *
+     * @return old time
+     */
+    public void setOldtimeofchangedcopy(double oldtime) {
+        this.oldtimeofchangedcopy = oldtime;
+    }
+
+    /**
+     * Obtain the new time of changed haplotype copy
+     *
+     * @return old time
+     */
+    public double getNewtimeofchangedcopy() {
+        return this.newtimeofchangedcopy;
+    }
+
+    /**
+     * Sets the old time of changed haplotype copy
+     *
+     * @return old time
+     */
+    public void setNewtimeofchangedcopy(double newtime) {
+        this.newtimeofchangedcopy = newtime;
     }
 
     /**

commit a01f839fd30de18ca10eb8f4d193b2f811c4e8ca

potential fix for incorrectly calculated likelihood issue introduced with def3799

src/piqmee/distributions/QuasiSpeciesBirthDeathSkylineModel.java
@@ -807,6 +807,11 @@ public class QuasiSpeciesBirthDeathSkylineModel extends BirthDeathSkylineModel {
 			System.arraycopy(Bi, 0, storedBi, 0, Bi.length);
 			System.arraycopy(logNumberOfQSTrees, 0, storedLogNumberOfQSTrees, 0, logNumberOfQSTrees.length);
 		}
+		
+		for (Node node: treeInput.get().getNodesAsArray()) {
+			((QuasiSpeciesNode)node).setOldtimeofchangedcopy(-1);
+		}
+		
 		super.store();
 	}
 