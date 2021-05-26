f51b389f5c3e3b2f6aa7b0a31c2f57063ee803ac
boskovav <2584478+boskovav@users.noreply.github.com>
2021-05-26 20:48:35 +0200

    fixed attachmet time loggers - were not working properly due to wrong node reference

---
+++ src/piqmee/util/AttachmentTimesAll.java
@@ -0,0 +1,84 @@
+package piqmee.util;
+
+import beast.core.*;
+import beast.evolution.tree.Node;
+import piqmee.tree.QuasiSpeciesTree;
+import piqmee.tree.QuasiSpeciesNode;
+
+import java.io.PrintStream;
+
+/**
+ * @author Veronika Boskova created on 03/08/15.
+ */
+@Description("Allows logging and attachment times of the haplotypes of a quasi-species tree.")
+public class AttachmentTimesAll extends CalculationNode implements Function, Loggable {
+
+    public Input<QuasiSpeciesTree> quasiSpeciesTreeInput = new Input<>(
+            "quasiSpeciesTree",
+            "Quasi-species tree whose attachment times should be logged.",
+            Input.Validate.REQUIRED);
+
+    private QuasiSpeciesTree qsTree;
+
+    public AttachmentTimesAll() { };
+
+    @Override
+    public void initAndValidate() {
+        qsTree = quasiSpeciesTreeInput.get();
+    }
+
+    @Override
+    public int getDimension() {
+        int size = 0;
+        for (Node node : qsTree.getExternalNodes()){
+            size += ((QuasiSpeciesNode) node).getAttachmentTimesList().length;
+        }
+        return size;
+    }
+
+    @Override
+    public double getArrayValue(int iDim) {
+        if (iDim < getDimension()) {
+            int index = iDim;
+            int size;
+            for (Node node : qsTree.getExternalNodes()){
+                size = ((QuasiSpeciesNode) node).getAttachmentTimesList().length;
+                if (index < size) {
+                    return ((QuasiSpeciesNode) node).getAttachmentTimesList()[index];
+                } else {
+                    index -= size;
+                }
+            }
+            return Double.NaN;
+        } else
+            return Double.NaN;
+    }
+
+    @Override
+    public void init(PrintStream out){
+
+        String idString = qsTree.getID();
+        // print all haplo names
+        for (Node node : qsTree.getExternalNodes()){
+            int maxtime = ((QuasiSpeciesNode) node).getAttachmentTimesList().length;
+            for (int time = 0; time < maxtime; time++) {
+                String haploname = node.getID();
+                out.print(idString + "_" + haploname + "_" + time + "\t");
+            }
+        }
+    }
+
+    @Override
+    public void log(int nSample, PrintStream out) {
+        // print all haplo names
+        for (Node node : qsTree.getExternalNodes()) {
+            double[] attachmentTimes = ((QuasiSpeciesNode) node).getAttachmentTimesList();
+            for (int time = 0; time < attachmentTimes.length; time++) {
+                out.print(attachmentTimes[time] + "\t");
+            }
+        }
+    }
+
+    @Override
+    public void close(PrintStream out) { }
+}
